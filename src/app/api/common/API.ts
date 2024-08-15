f8882d40f0b917f2407dff01f00241cbad37300f
Hyunji_ <hyunji1443@gmail.com>
2024-08-15 23:07:08 +0900

    map-v4

--- src/app/api/common/API.ts
+++ src/app/api/common/API.ts
@@ -1,8 +1,17 @@
+// export const enum API{
+//     TXSERVER = 'http://localhost:8000/tx',
+//     USERSERVER = 'http://localhost:8000/users',
+//     LANDSERVER = 'http://localhost:8000/land/api',
+//     ANALYSERVER = 'http://localhost:8000/analysis',
+//     CHATSERVER = 'http://localhost:8000/chat',
+//     GATE = 'http://localhost:8000'
+// }
+
 export const enum API{
-    TXSERVER = 'http://localhost:8000/tx',
-    USERSERVER = 'http://localhost:8000/users',
-    LANDSERVER = 'http://localhost:8000/land/api',
-    ANALYSERVER = 'http://localhost:8000/analysis',
-    CHATSERVER = 'http://localhost:8000/chat',
-    GATE = 'http://localhost:8000'
+    TXSERVER = 'https://api.eomhyunji.bangez.com/tx',
+    USERSERVER = 'https://api.eomhyunji.bangez.com/users',
+    LANDSERVER = 'https://api.eomhyunji.bangez.com/land/api',
+    ANALYSERVER = 'https://api.eomhyunji.bangez.com/analysis',
+    CHATSERVER = 'https://api.eomhyunji.bangez.com/chat',
+    GATE = 'https://api.eomhyunji.bangez.com'
 }
--- src/components/listing/map-style/map-v4/FeatuerdListings.js
+++ src/components/listing/map-style/map-v4/FeatuerdListings.js
@@ -1,40 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
-import img01 from '../../../../../public/Images/listings/make/1.jpg'; //이미지 하드
-import img02 from '../../../../../public/Images/listings/make/2.jpg'; 
-import img03 from '../../../../../public/Images/listings/make/3.jpg'; 
-import img04 from '../../../../../public/Images/listings/make/4.jpg'; 
-import img05 from '../../../../../public/Images/listings/make/5.jpg'; 
-import img06 from '../../../../../public/Images/listings/make/6.jpg'; 
-import img07 from '../../../../../public/Images/listings/make/7.jpg'; 
-import img08 from '../../../../../public/Images/listings/make/8.jpg'; 
-import img09 from '../../../../../public/Images/listings/make/9.jpg'; 
-import img10 from '../../../../../public/Images/listings/make/10.jpg'; 
-import img11 from '../../../../../public/Images/listings/make/11.jpg'; 
-import img12 from '../../../../../public/Images/listings/make/12.jpg'; 
-import img13 from '../../../../../public/Images/listings/make/13.jpg'; 
-import img14 from '../../../../../public/Images/listings/make/14.jpg'; 
-import img15 from '../../../../../public/Images/listings/make/15.jpg'; 
-import img16 from '../../../../../public/Images/listings/make/16.jpg'; 
-import img17 from '../../../../../public/Images/listings/make/17.jpg'; 
-import img18 from '../../../../../public/Images/listings/make/18.jpg'; 
-import img19 from '../../../../../public/Images/listings/make/19.jpg'; 
-import img20 from '../../../../../public/Images/listings/make/20.jpg'; 
-import img21 from '../../../../../public/Images/listings/make/21.jpg'; 
-import img22 from '../../../../../public/Images/listings/make/22.jpg'; 
-import img23 from '../../../../../public/Images/listings/make/23.jpg'; 
+// import img01 from '../../../../../public/Images/listings/make/1.jpg'; //이미지 하드
+// import img02 from '../../../../../public/Images/listings/make/2.jpg'; 
+// import img03 from '../../../../../public/Images/listings/make/3.jpg'; 
+// import img04 from '../../../../../public/Images/listings/make/4.jpg'; 
+// import img05 from '../../../../../public/Images/listings/make/5.jpg'; 
+// import img06 from '../../../../../public/Images/listings/make/6.jpg'; 
+// import img07 from '../../../../../public/Images/listings/make/7.jpg'; 
+// import img08 from '../../../../../public/Images/listings/make/8.jpg'; 
+// import img09 from '../../../../../public/Images/listings/make/9.jpg'; 
+// import img10 from '../../../../../public/Images/listings/make/10.jpg'; 
+// import img11 from '../../../../../public/Images/listings/make/11.jpg'; 
+// import img12 from '../../../../../public/Images/listings/make/12.jpg'; 
+// import img13 from '../../../../../public/Images/listings/make/13.jpg'; 
+// import img14 from '../../../../../public/Images/listings/make/14.jpg'; 
+// import img15 from '../../../../../public/Images/listings/make/15.jpg'; 
+// import img16 from '../../../../../public/Images/listings/make/16.jpg'; 
+// import img17 from '../../../../../public/Images/listings/make/17.jpg'; 
+// import img18 from '../../../../../public/Images/listings/make/18.jpg'; 
+// import img19 from '../../../../../public/Images/listings/make/19.jpg'; 
+// import img20 from '../../../../../public/Images/listings/make/20.jpg'; 
+// import img21 from '../../../../../public/Images/listings/make/21.jpg'; 
+// import img22 from '../../../../../public/Images/listings/make/22.jpg'; 
+// import img23 from '../../../../../public/Images/listings/make/23.jpg'; 
 
-// 모든 이미지를 배열에 추가
-const images = [
-  img01, img02, img03, img04, img05, img06, img07, img08, img09, img10,
-  img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
-  img21, img22, img23
-];
+// // 모든 이미지를 배열에 추가
+// const images = [
+//   img01, img02, img03, img04, img05, img06, img07, img08, img09, img10,
+//   img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
+//   img21, img22, img23
+// ];
 
-// 랜덤 이미지 선택 함수
+// // 랜덤 이미지 선택 함수
+// const getRandomImage = () => {
+//   const randomIndex = Math.floor(Math.random() * images.length);
+//   return images[randomIndex];
+// };
+
 const getRandomImage = () => {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  return images[randomIndex];
+  const randomIndex = Math.floor(Math.random() * 23) + 1; // 1부터 23까지의 랜덤 숫자 생성
+  return `/images/listings/make/${randomIndex}.jpg`;
 };
 
 const FeaturedListings = ({ data, colstyle }) => {
